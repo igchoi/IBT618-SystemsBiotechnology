c9bb9dfbff79ea52673aa52b34fe004c542eceab
In-Geol Choi <igchoi@gmail.com>
2024-04-09 16:27:41 +0900

    Add files via upload

---
+++ 2024-spring/sandbox/tmp.R
@@ -0,0 +1,11 @@
+# test
+tmp = c(1:10)
+print(tmp)
+setwd("C:/Users/choil/Documents/class")
+setwd("C:/Users/choil/Documents/class/msan")
+?setwd()
+library(tidyverse)
+#install.pacakges("tidyverse")
+
+## test ##
+
